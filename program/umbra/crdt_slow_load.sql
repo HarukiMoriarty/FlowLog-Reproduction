814370dc2d291e29bb878453084c52ab18b74e18
hangdong <hangdong@node0.rebuttal2.lpad-pg0.clemson.cloudlab.us>
2025-08-08 19:59:41 +0000

    add umbra crdt_slow sql

---
+++ program/umbra/crdt_slow_load.sql
@@ -0,0 +1,21 @@
+-- Based on crdt_slow.dl Datalog program
+-- CRDT (Conflict-free Replicated Data Type) implementation for ordered lists
+
+-- Create input tables
+DROP TABLE IF EXISTS Insert_input;
+CREATE TABLE Insert_input (
+    a INTEGER,
+    b INTEGER, 
+    c INTEGER,
+    d INTEGER
+);
+
+DROP TABLE IF EXISTS Remove_input;
+CREATE TABLE Remove_input (
+    a INTEGER,
+    b INTEGER
+);
+
+-- Load data from CSV files
+COPY Insert_input FROM '{{DATASET_PATH}}/Insert_input.csv' (FORMAT CSV, HEADER FALSE);
+COPY Remove_input FROM '{{DATASET_PATH}}/Remove_input.csv' (FORMAT CSV, HEADER FALSE);
